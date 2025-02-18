da80dca46f81ada79b1c14ea36b04f851542210b
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-02-18 20:22:35 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (57.81%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    bool mathaches(string & res , string & pattern){
-        for(int i = 0 ;i < pattern.length();i++){
-            if((pattern[i] == 'I' && res[i] > res[i+1])||
-               (pattern[i] == 'D' && res[i] < res[i+1])){
-                   return 0;
-            }
-        }
-        return 1;
-    }
     string smallestNumber(string pattern) {
         int n = pattern.length();
-        string res ="";
-        for(int i =1 ; i <= n+1 ; i++){
-           res.push_back(i+'0'); 
-        }
+        string res = "";
+        stack<char>st;
+        int counter = 1;
 
-        while(!mathaches(res,pattern)){
-            next_permutation(res.begin(),res.end());
+        for(int i = 0 ; i <= n; i++){
+            
+             st.push(counter+'0');
+             counter++;
+             if(i == n || pattern[i] == 'I'){
+                  while(!st.empty()){
+                    res += st.top();
+                    st.pop();
+                  }
+             }
         }
         return res;
     }
