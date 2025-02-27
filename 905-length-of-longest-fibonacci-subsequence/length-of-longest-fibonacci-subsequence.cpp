407ba7a221a7b697161f396d78f2555f383c9df3
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-02-27 20:21:08 +0530

    Time: 347 ms (39.31%) | Memory: 150 MB (21.37%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    int solve(int i , int j , vector<int>&arr,unordered_map<int,int>&EleToInd){
-        int target = arr[j] - arr[i];
-        if(EleToInd.count(target) && EleToInd.at(target) < i){
-            int k = EleToInd.at(target);
-            return solve(k,i,arr,EleToInd) + 1;
-        }
-       return 2;
-    }
     int lenLongestFibSubseq(vector<int>& arr) {
         int n = arr.size();
         unordered_map<int,int>EleToInd;
-
+        vector<vector<int>>dp(n,vector<int>(n,2));
         for(int i = 0 ; i < arr.size();i++){
             EleToInd[arr[i]] = i;
         }
         int result = 0;
         for(int i = 1 ; i < n ; i++){
             for(int j = i+1 ; j < n ; j++){
-               
-                    int len = solve(i,j,arr,EleToInd);
-                    if(len >= 3){
-                        result = max(len,result);
+                    int target = arr[j] - arr[i];
+                    if(EleToInd.count(target) && EleToInd[target] < i){
+                        int k = EleToInd[target];
+                        dp[i][j] = dp[k][i] + 1;
                     }
+                result = max(result , dp[i][j]);
             }  
 
         }
-        return result ;
+        return result >= 3 ? result :0;
     }
 };
