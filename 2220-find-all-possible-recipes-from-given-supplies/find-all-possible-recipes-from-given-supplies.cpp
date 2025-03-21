5533983600842c9b70ce063e48c2cd5bf3611b20
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-03-21 15:45:41 +0530

    Time: 26 ms (97.73%) | Memory: 101 MB (89.42%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -2,29 +2,40 @@ class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
         int n = recipes.size();
-        unordered_set<string>mpp(supplies.begin(),supplies.end()); // for quick check;
+        unordered_set<string>mpp(supplies.begin(),supplies.end());
+
+        unordered_map<string, vector<int>>adj;
+        vector<int>indegree(n,0);
         vector<string>res;
-        vector<bool>cooked(n,false);
-        int c = n;
-        while(c--){
-            for(int j = 0 ; j < n ; j++){
-                if(cooked[j]) continue;
 
-                bool ok = true;
+        for(int i = 0 ; i < n ; i++){
+             for(string ing : ingredients[i]){
+                  if(!mpp.count(ing)){
+                    adj[ing].push_back(i);
+                    indegree[i]++;
+                  }
+             }
+        }
 
-                for(int k = 0 ; k < ingredients[j].size() ; k++){
-                    if(mpp.find(ingredients[j][k]) == mpp.end()){
-                        ok = false;
-                        break;
-                    }
-                }
-                if(ok){
-                    mpp.insert(recipes[j]);
-                    res.push_back(recipes[j]);
-                    cooked[j] = true;
-                }
+        queue<int>q;
+        for(int i = 0 ; i < n ; i++){
+            if(indegree[i] == 0){
+                q.push(i);
             }
         }
-      return res;
+
+        while(!q.empty()){
+            auto i = q.front();
+            q.pop();
+            res.push_back(recipes[i]);
+            
+            for(auto ind : adj[recipes[i]]){
+                 indegree[ind]--;
+                 if(indegree[ind] == 0){
+                    q.push(ind);
+                 }
+            }
+        } 
+        return res;
     }
 };
