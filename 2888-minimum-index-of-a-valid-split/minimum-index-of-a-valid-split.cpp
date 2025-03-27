8ce2e0155b4442bd22052e39763855098ab836e5
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-03-27 20:21:29 +0530

    Time: 0 ms (100.00%) | Memory: 89.8 MB (85.66%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int findMajorityElement(vector<int>& nums) {
+    int candidate = 0, count = 0;
+
+    for (int num : nums) {
+        if (count == 0) {
+            candidate = num;
+        }
+        count += (num == candidate) ? 1 : -1;
+    }
+    count = 0;
+    for (int num : nums) {
+        if (num == candidate) {
+            count++;
+        }
+    }
+
+    if (count > nums.size() / 2) {
+        return candidate;
+    }
+
+    return -1; 
+   }
+    int minimumIndex(vector<int>& nums) {
+    int n = nums.size();
+    
+    int dominant = findMajorityElement(nums);
+    int totalCount = 0;
+    for (int num : nums) {
+        if (num == dominant) {
+            totalCount++;
+        }
+    }
+
+    int leftCount = 0;
+    for (int i = 0; i < n - 1; i++) {
+        if (nums[i] == dominant) {
+            leftCount++;
+        }
+        
+        int leftSize = i + 1;
+        int rightCount = totalCount - leftCount;
+        int rightSize = n - leftSize;
+
+        if (leftCount * 2 > leftSize && rightCount * 2 > rightSize) {
+            return i;
+        }
+    }
+
+    return -1;
+    }
+};
