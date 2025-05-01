f41d6f15eec95fb2eb358c30c10fa68c4f6b1b57
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-05-01 18:23:19 +0530

    Time: 25 ms (85.34%) | Memory: 80.3 MB (24.63%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    bool possible(int k , vector<int>&players,vector<int>&trainers){
-        multiset<int>ml(trainers.end()-k , trainers.end());
-        for(int i = k - 1 ; i >= 0 ; i--){
-            auto it = ml.lower_bound(players[i]);
-            if(it == ml.end()) return false;
-            ml.erase(it);
-       }
-      return true;
-    }
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
         sort(players.begin(),players.end());
         sort(trainers.begin(),trainers.end());
 
-        int left = 0 , right = min(players.size(),trainers.size());
-        int ans = 0;
-        while(left <= right){
-             int mid = left+(right - left)/2;
-
-             if(possible(mid,players,trainers)){
-                ans = mid;
-                left = mid + 1;
-             }else{
-                right = mid - 1;
-             }
+        int i = 0 , j = 0 , count = 0;
+        while(i < players.size() && j < trainers.size()){
+              if(players[i] <= trainers[j]){
+                count ++;
+                i++; j++;
+              }else{
+                 j++;
+              }
         }
-      return ans;
+        return count;
     }
 };
