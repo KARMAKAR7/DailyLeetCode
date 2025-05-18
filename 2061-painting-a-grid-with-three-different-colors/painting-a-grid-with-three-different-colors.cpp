852a7a116039ed9bca265c7e24734fa3af70ec0b
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-05-18 16:22:56 +0530

    Time: 346 ms (18.39%) | Memory: 15.9 MB (44.25%) - LeetSync

---
+++ 2061-painting-a-grid-with-three-different-colors/painting-a-grid-with-three-different-colors.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    const int mod = 1e9+7;
+    vector<string>preprocessing;
+    vector<vector<int>>dp;
+    void pre(string curr , char prev , int len , int r){
+        if(len == r){
+            preprocessing.push_back(curr);
+            return;
+        }
+        for(char ch:{'R','G','B'}){
+            if(prev == ch){
+                continue;
+            }
+            pre(curr+ch , ch , len+1,r);
+        }
+    }
+    bool valid(string &curr , string &prev,int m){
+        for(int j = 0 ; j < m ; j++){
+            if(curr[j]==prev[j]){
+                return false;
+            }
+        }
+       return true;
+    }
+    int solve(int rem , int prev,int m){
+        if(rem == 0){
+            return 1;
+        }
+        if(dp[rem][prev] != -1){
+            return dp[rem][prev];
+        }
+        int ways = 0;
+        string p = preprocessing[prev];
+        for(int  i = 0 ; i < preprocessing.size() ; i++){
+             if(i == prev) continue;
+             string curr = preprocessing[i];
+             if(valid(curr,p,m)){
+                ways = (ways + solve(rem-1,i,m))%mod;
+             }
+        }
+      return dp[rem][prev] = ways;
+    }
+    int colorTheGrid(int m, int n) {
+         pre("",'#',0,m);
+         dp = vector<vector<int>>(n+1,vector<int>(preprocessing.size()+1,-1));
+         int res = 0;
+         for(int i = 0 ; i < preprocessing.size();i++){
+            res = (res + solve(n-1,i,m))%mod;
+         }
+         return res;
+    }
+};
