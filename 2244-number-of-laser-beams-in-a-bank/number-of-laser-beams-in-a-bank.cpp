0f86d289ef6c451ee81582d99ba27c79a65e8114
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-05-21 20:48:24 +0530

    Time: 15 ms (17.27%) | Memory: 27.4 MB (69.42%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    int numberOfBeams(vector<string>& bank) {
-        vector<int> devicesPerRow;
-        
-        for (const string& row : bank) {
-            int count = count_if(row.begin(), row.end(), [](char c) { return c == '1'; });
-            if (count > 0) {
-                devicesPerRow.push_back(count);
-            }
+    int rowOneCount(const string &s){
+        int count = 0;
+        for(char c : s){
+            if(c == '1') count++;
         }
+        return count;
+    } 
 
-        int totalBeams = 0;
-        for (int i = 1; i < devicesPerRow.size(); ++i) {
-            totalBeams += devicesPerRow[i - 1] * devicesPerRow[i];
-        }
-
-        return totalBeams;
+   int numberOfBeams(vector<string>& bank) {
+    vector<int> device;
+    for(const auto& row : bank){
+        int c = rowOneCount(row);
+        if(c > 0) device.push_back(c);
     }
+
+    int ans = 0;
+    if (device.size() >= 2) {
+        for(int i = 0 ; i < device.size() - 1; i++){
+            ans += device[i] * device[i + 1];
+        }
+    }
+
+    return ans;
+}
 };
