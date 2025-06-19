2e542885f68f34938abf722c3fc900d4127fb46f
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-06-19 18:12:07 +0530

    Time: 1463 ms (69.15%) | Memory: 358.2 MB (31.92%) - LeetSync

---
+++ 3798-longest-special-path-ii/longest-special-path-ii.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int longest_path = 0 ;
+    int minCount = INT_MAX;
+    int n;
+    vector<vector<pair<int, int>>> adj; 
+    vector<int>trace;
+    void dfs(int node , int par , int s , int e , int pathSum , vector<int>&path , vector<int>&nums,int twice){
+        int prev = trace[nums[node]];
+        trace[nums[node]] = e;
+
+        while(s <= min(prev,twice)){
+             pathSum -= path[s];
+             s++;
+        }
+
+        if(prev != -1){
+            twice = max(prev , twice);
+        }
+
+        if(pathSum > longest_path){
+             longest_path = pathSum;
+             minCount = e - s + 1;
+        }else if(pathSum == longest_path){
+              minCount = min(minCount  , e - s + 1);
+        }
+
+        for(auto &it: adj[node]){
+             if(it.first != par){
+                 path.push_back(it.second);
+                 dfs(it.first , node , s , e+1 , pathSum+ it.second ,path , nums , twice);
+                 path.pop_back();
+             }
+        }
+        trace[nums[node]] = prev;
+    }
+    vector<int> longestSpecialPath(vector<vector<int>>& edges, vector<int>& nums) {
+        n = edges.size()+1;
+        adj.resize(n);
+        trace.resize(50001, -1);
+        for(auto &e : edges){
+            int u = e[0];
+            int v = e[1];
+            int ln = e[2];
+            adj[u].push_back({v,ln});
+            adj[v].push_back({u,ln});
+        }
+
+        vector<int>path;
+        dfs(0,-1,0,0,0,path,nums,-1);
+        return {longest_path , minCount};
+    }
+};
