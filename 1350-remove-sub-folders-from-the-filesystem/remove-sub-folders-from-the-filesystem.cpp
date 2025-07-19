97949053939697f21d83273a0db2a0ac24203f05
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-07-19 15:45:27 +0530

    Time: 41 ms (92.47%) | Memory: 47.5 MB (82.80%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,68 +1,19 @@
 class Solution {
 public:
-    struct TrieNode{
-        bool isEndOfWord;
-        vector<TrieNode*>children;
-
-        TrieNode(){
-            this->isEndOfWord = false;
-            this->children = vector<TrieNode*>(27,NULL);
-        }
-    };
-
-    void insertWord(TrieNode *root,string word){
-        TrieNode*crawl = root;
-        for(char c:word){
-            if(c == '/'){
-                if(crawl -> children[26] == NULL){
-                    crawl->children[26] = new TrieNode();
-                }
-                crawl = crawl->children[26];
-            }else{
-                int index = c - 'a';
-                if(crawl -> children[index] == NULL){
-                    crawl->children[index] = new TrieNode();
-                }
-                crawl = crawl -> children[index];
-            }
-        }
-        crawl -> children[26] = new TrieNode();
-        crawl = crawl -> children[26];
-        crawl->isEndOfWord = true;
-    }
-
-    bool checkWord(TrieNode*root , string word){
-        TrieNode* crawl = root;
-        for(char c : word){
-         if(c == '/'){
-            if(crawl->children[26] == NULL) return true;
-            crawl = crawl -> children[26];
-         }else{
-            int index = c - 'a';
-            if(crawl->isEndOfWord == true){
-                return false;
-            }
-            if(crawl->children[index] == NULL){
-                return true;
-            }
-            crawl = crawl ->children[index];
-           }
-        } 
-       return true;
-    }
-    void insertAndCheck(TrieNode* root , string word , vector<string>&ans){
-        if(checkWord(root,word)){
-            insertWord(root,word);
-            ans.push_back(word);
-        }
-    }
     vector<string> removeSubfolders(vector<string>& folder) {
-       TrieNode * root = new TrieNode();
-       vector<string>ans;
-       sort(folder.begin(),folder.end());
-        for(string word:folder){
-            insertAndCheck(root,word,ans);
-        }
-       return ans;
+        int n = folder.size();
+        vector<string>res;
+        sort(folder.begin(),folder.end());
+
+        res.push_back(folder[0]);
+
+        for(int i = 1 ; i < n ; i++){
+            string last = res.back() + '/';
+
+            if(folder[i].find(last) !=  0){
+                res.push_back(folder[i]);
+            } 
+        } 
+        return res;
     }
 };
