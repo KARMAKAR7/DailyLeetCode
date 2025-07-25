17b1f92dcf72ba6e027ff5143b94b83cac36578c
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-07-25 23:10:46 +0530

    Time: 1 ms (67.85%) | Memory: 9.7 MB (88.42%) - LeetSync

--- 3243-count-the-number-of-powerful-integers/count-the-number-of-powerful-integers.cpp
+++ 3243-count-the-number-of-powerful-integers/count-the-number-of-powerful-integers.cpp
@@ -1,36 +1,52 @@
 class Solution {
 public:
-    long long solve(const string& str, const string& suffix, int limit) {
-        if (str.length() < suffix.length())
-            return 0;
+    #define ll long long
+    ll dp[16][2];
+    ll fun(int index,bool tight , int &maxAllowed,string &suffix , string &s){
+        if(index == s.size()){
+            return 1;
+        }
+       if(dp[index][tight] != -1){
+            return dp[index][tight];
+       }
+    ll ans = 0;
+    int limit = tight ? s[index]-'0' : 9;
+    limit = min(limit,maxAllowed);
 
-        if (str.length() == suffix.length())
-            return str >= suffix ? 1 : 0;
+    int suffixIndex = s.size() - suffix.size();
 
-        long long count = 0;
-        string trailing = str.substr(str.length() - suffix.length());
-        int prefixLength = str.length() - suffix.length();
-
-        for (int i = 0; i < prefixLength; ++i) {
-            int digit = str[i] - '0';
-
-            if (limit < digit) {
-                count += pow(limit + 1, prefixLength - i);
-                return count;
-            } else {
-                count += digit * pow(limit + 1, prefixLength - i - 1);
-            }
+    if(index >= suffixIndex){
+        if(tight == false){
+            return dp[index][tight] = 1;
         }
 
-        if (trailing >= suffix)
-            count++;
-
-        return count;
+        if(s[index] < suffix[index-suffixIndex]){
+            return dp[index][tight] = 0;
+        }
+        if(s[index] == suffix[index-suffixIndex]){
+            return dp[index][tight] = fun(index+1,true,maxAllowed , suffix , s);
+        }
+        if(s[index] > suffix[index-suffixIndex]){
+            return dp[index][tight] = 1;
+        }
     }
+    for(int d = 0 ; d <= limit ; d++){
+        int newTight = tight & (d == s[index]-'0');
+        ans += fun(index+1,newTight,maxAllowed,suffix,s);
+    }
+    return dp[index][tight] = ans;
+    }
+    long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
+        ll count1 = 0;
+        string l = to_string(start - 1);
+        if(l.size() >= s.size()){
+            memset(dp,-1,sizeof(dp));
+            count1 = fun(0,true,limit,s,l);
+        }
 
-    long long numberOfPowerfulInt(long long start, long long finish, int limit, const string& s) {
-        string startStr  = to_string(start - 1);
-        string finishStr = to_string(finish);
-        return solve(finishStr, s, limit) - solve(startStr, s, limit);
+        string h = to_string(finish);
+        memset(dp,-1LL,sizeof(dp));
+        ll count2 = fun(0,true,limit,s,h);
+        return count2 - count1;
     }
 };
