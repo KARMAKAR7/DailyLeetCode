be0b24d4235fe2226e5fd314f9769580a168d839
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-08-11 14:58:40 +0530

    Time: 22 ms (55.85%) | Memory: 147.8 MB (50.53%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -8,15 +8,30 @@ public:
             }
         }
     }
+    long long modPow(long long base, long long exp) {
+        long long res = 1;
+        while (exp > 0) {
+            if (exp & 1) res = (res * base) % MOD;
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+        return res;
+    }
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
         vector<int>ans;
         vector<int>pow;
         pre(n,pow);
+        int m = pow.size();
+        vector<long long> prefix(m);
+        prefix[0] = pow[0] % MOD;
+        for (int i = 1; i < m; i++) {
+            prefix[i] = (prefix[i - 1] * pow[i]) % MOD;
+        }
         for (auto &q : queries) {
             int l = q[0], r = q[1];
-            long long prod = 1;
-            for (int i = l; i <= r; i++) {
-                prod = (prod * pow[i]) % MOD;
+            long long prod = prefix[r];
+            if (l > 0) {
+                prod = (prod * modPow(prefix[l - 1], MOD - 2)) % MOD; // modular inverse
             }
             ans.push_back((int)prod);
         }
