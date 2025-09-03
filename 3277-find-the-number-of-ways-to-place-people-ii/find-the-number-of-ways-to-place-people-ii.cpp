e83ddd8cad38284e7c9e0d9392683e26dd0f4099
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-09-03 18:30:59 +0530

    Time: 136 ms (51.06%) | Memory: 41.3 MB (19.15%) - LeetSync

---
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+    int n = points.size();
+       sort(points.begin(),points.end(),[](const vector<int>&a , const vector<int>&b){
+         return a[0] == b[0] ? a[1] >b[1] : a[0] < b[0];
+       });   
+       int numnbersValid = 0;
+       for(int top = 0 ; top < n - 1 ; top++){
+           int btm = INT_MIN; // find the maximum buttom point
+           for(int j = top+1 ; j < n ; j++){
+                 if(points[j][1] <= points[top][1] && points[j][1] > btm){
+                    numnbersValid++;
+                    btm =  points[j][1];
+                 }
+           }
+       }
+       return numnbersValid;
+    }
+};
