2cd5c56e2a184477f1a008fc23cabccea5b908e3
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-10-13 07:41:09 +0530

    Time: 21 ms (5.06%) | Memory: 21.6 MB (7.37%) - LeetSync

---
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+   bool ana(string &s1 , string &s2){
+       unordered_map<char,int>f;
+       for(auto i:s1){
+            f[i]++;
+       }
+       for(auto i:s2){
+           f[i]--;
+           if(f[i]==0){
+               f.erase(i);
+           }
+       }
+       return f.empty();
+   }
+    vector<string> removeAnagrams(vector<string>& words) {
+        int n = words.size();
+        vector<string>ans;
+        for(int i = 0; i < n; i++){
+             if(!ans.empty() && ana(ans.back(),words[i])){
+                //  ans.push_back(words[i]);
+                continue;
+             }else{
+                ans.push_back(words[i]);
+             }
+        }
+    return ans;
+    }
+};
