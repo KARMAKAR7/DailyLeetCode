61716954dbcf8a82625acafc2c1bf0737a74a8e2
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-11-12 08:11:51 +0530

    Time: 0 ms (100.00%) | Memory: 31.7 MB (72.22%) - LeetSync

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -8,16 +8,25 @@ public:
         if(g != 1){
             return -1; // impossible case
         }
-        // may be array can have ones so no oprtaions needed for the,
+        // may be array can have ones so no oprtaions needed for them,
         int ones = 0;
         for (int x : nums) if (x == 1) ++ones;
         if (ones > 0) return nums.size() - ones;
-
+        /*
+           the core thing is wee need to find a subarray that have gcd == 1
+           if we find the subarry with gcd == 1
+           the we can pass this to adject elements
+           so operation will be 
+           
+           // first we need to convert all the elemnts of in subarray with gcd == 1
+           for them reuired operations will be there lenngth
+           + the other elemnts that are not in this subarray 
+        */
         int ans = nums.size()+1;
         for(int i = 0 ; i < nums.size() ; i++){
                int gc = 0;
                for(int j = i ; j < nums.size() ; j++){
-                      gc = gcd(gc,nums[j]);
+                      gc = __gcd(gc,nums[j]);
                       if(gc == 1){  // we found gcd == 1
                           ans = min(ans,j-i);
                       }
