80b7bedf6ed84591ac29fbd34633e63d4d77bb68
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2026-01-03 12:24:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (86.67%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -1,51 +1,17 @@
 class Solution {
+    int mod = 1e9 + 7;
 public:
-    const int mod = 1e9+7;
-    vector<string>preprocessing;
-    vector<vector<int>>dp;
-    void pre(string curr , char prev , int len , int r){
-        if(len == r){
-            preprocessing.push_back(curr);
-            return;
-        }
-        for(char ch:{'R','Y','G'}){
-            if(prev == ch){
-                continue;
-            }
-            pre(curr+ch , ch , len+1,r);
-        }
-    }
-     bool valid(string &curr , string &prev,int m){
-        for(int j = 0 ; j < m ; j++){
-            if(curr[j]==prev[j]){
-                return false;
-            }
-        }
-       return true;
-    }
-     int solve(int rem , int prev,int m){
-        if(rem == 0){
-            return 1;
-        }
-        if (dp[rem][prev] != -1) return dp[rem][prev];
-        int ways = 0;
-        string p = preprocessing[prev];
-        for(int  i = 0 ; i < preprocessing.size() ; i++){
-             if(i == prev) continue;
-             string curr = preprocessing[i];
-             if(valid(curr,p,m)){
-                ways = (ways + solve(rem-1,i,m))%mod;
-             }
-        }
-      return dp[rem][prev] = ways;
-    }
     int numOfWays(int n) {
-        pre("",'#',0,3);
-        dp = vector<vector<int>>(n+1, vector<int>(preprocessing.size()+1, -1));
-        int res = 0;
-         for(int i = 0 ; i < preprocessing.size();i++){
-            res = (res + solve(n-1,i,3))%mod;
-         }
-         return res;
+        long long diff = 6, same = 6;
+
+        for(int i = 1; i < n; i++) {
+            long long s = (3*same + 2*diff)%mod;
+            long long d = (2*same + 2*diff)%mod;
+
+            same = s;
+            diff = d; 
+        }
+
+        return (same + diff)%mod;
     }
 };
