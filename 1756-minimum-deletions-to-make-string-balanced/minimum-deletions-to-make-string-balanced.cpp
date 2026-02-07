40cb9f1271eaae0573ec9d75bd3540ead77fe47c
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2026-02-07 13:34:26 +0530

    Time: 19 ms (73.82%) | Memory: 25.6 MB (54.88%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+          int bCount = 0;     // number of 'b' seen so far
+        int deletions = 0;  // minimum deletions
+
+        for(char c : s) {
+            if(c == 'b') {
+                bCount++;
+            } else { // c == 'a'
+                if(bCount > 0) {
+                    deletions++;
+                    bCount--;   // delete one previous 'b'
+                }
+            }
+        }
+        return deletions;
+    }
+};
