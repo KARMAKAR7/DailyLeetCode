8d47031aeefdf5962b2c281bf0b1c111152ad31e
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2026-04-04 12:02:34 +0530

    Time: 36 ms (48.30%) | Memory: 41.8 MB (63.56%) - LeetSync

---
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+       int n = encodedText.size();
+        if (rows == 0) return "";
+        
+        int cols = n / rows;
+        string result = "";
+        
+        // Traverse diagonally
+        for (int startCol = 0; startCol < cols; startCol++) {
+            int i = 0, j = startCol;
+            
+            while (i < rows && j < cols) {
+                result += encodedText[i * cols + j];
+                i++;
+                j++;
+            }
+        }
+          // Remove trailing spaces
+        while (!result.empty() && result.back() == ' ') {
+            result.pop_back();
+        }
+        
+        return result;   
+    }
+};
