f2ba86047857c659875c7682144a4369515802de
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2026-04-14 20:43:55 +0530

    Time: 33 ms (58.90%) | Memory: 13.4 MB (77.40%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -2,29 +2,46 @@ class Solution {
 public:
     #define ll long long
     vector<vector<ll>> dp;
-    ll solve(int robotIdx , int factoryIdx , vector<int>&robot , vector<int>&FactPositions,int n , int m){
-        if(robotIdx == n) return 0;
-        if(factoryIdx == m) return 1e12;
-        if(dp[robotIdx][factoryIdx] != -1) return dp[robotIdx][factoryIdx]; 
-        ll take = abs(robot[robotIdx] - FactPositions[factoryIdx]) + solve(robotIdx+1 , factoryIdx+1 , robot,FactPositions,n,m);
-        ll skip = solve(robotIdx ,factoryIdx+1 , robot , FactPositions,n,m);
 
-        return dp[robotIdx][factoryIdx]= min(take,skip);
-    }
-    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+    ll solve(int i, int j, vector<int>& robot, vector<vector<int>>& factory) {
         int n = robot.size();
-        // sort the robots and factories by their positions becuase we need to find the near factory
-        sort(robot.begin(),robot.end());
-        sort(factory.begin(),factory.end());
+        int m = factory.size();
 
-        vector<int>FactPositions;
-        for(auto fact:factory){
-            for(int i = 0 ; i < fact[1] ; i++){
-                FactPositions.push_back(fact[0]);
-            }
+        // all robots assigned
+        if (i == n) return 0;
+
+        // no factories left
+        if (j == m) return 1e15;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        ll res = 1e15;
+
+        // Option 1: skip this factory
+        res = solve(i, j + 1, robot, factory);
+
+        // Option 2: take k robots from this factory
+        ll cost = 0;
+        int pos = factory[j][0];
+        int limit = factory[j][1];
+
+        for (int k = 0; k < limit && i + k < n; k++) {
+            cost += abs(robot[i + k] - pos);
+            res = min(res, cost + solve(i + k + 1, j + 1, robot, factory));
         }
-        int m = FactPositions.size();
+
+        return dp[i][j] = res;
+    }
+
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        sort(robot.begin(), robot.end());
+        sort(factory.begin(), factory.end());
+
+        int n = robot.size();
+        int m = factory.size();
+
         dp.assign(n + 1, vector<ll>(m + 1, -1));
-        return solve(0,0,robot,FactPositions,n,m);
+
+        return solve(0, 0, robot, factory);
     }
 };
